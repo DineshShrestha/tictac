eb7437f3bd89903f3b74f3bb8129e4946a473800
sdin <sdin@LAPTOP-DINESH>
2024-01-04 16:07:09 +0100

    added function for placing piece and play_at board

--- lib/tictac.ex
+++ lib/tictac.ex
@@ -8,7 +8,20 @@ defmodule Tictac do
       _    -> {:error, :invalid_player}
     end
   end
-
+  def place_piece(board, place, player) do
+    case board[place] do
+      nil     -> {:error, :invalid_location}
+      :x      -> {:error, :occupied}
+      :o      -> {:error, :occupied}
+      :empty  -> {:ok, %{board | place => player}}
+    end
+  end
+  def play_at(board, col, row, valid_player) do
+    with {:ok, valid_player}  <- check_player(valid_player),
+          {:ok, square}       <- Square.new(col, row),
+          {:ok, new_board}    <- place_piece(board, square, valid_player),
+      do: new_board
+  end
   def new_board do
     for s <- squares(), into: %{}, do: {s, :empty}
   end

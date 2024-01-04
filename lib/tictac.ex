858e4b9344c5f569429febd12604fddbeb147d86
sdin <sdin@LAPTOP-DINESH>
2024-01-04 14:58:23 +0100

    adding new board and players option

--- lib/tictac.ex
+++ lib/tictac.ex
@@ -1,4 +1,17 @@
 defmodule Tictac do
+  @players {:x, :o}
+
+  def check_player(player) do
+    case player do
+      :x    -> {:ok, player}
+      :o    -> {:ok, player}
+      _    -> {:error, :invalid_player}
+    end
+  end
+
+  def new_board do
+    for s <- squares(), into: %{}, do: {s, :empty}
+  end
   def squares do
     for c<- 1..3, r <- 1..3, into: MapSet.new(), do: %Square{col: c, row: r}
   end

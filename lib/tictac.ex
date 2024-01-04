59b2035fbe448eb7d189ae2c2148106f46e4726e
sdin <sdin@LAPTOP-DINESH>
2024-01-04 14:45:16 +0100

    added squares function for initial setup of row and column

--- lib/tictac.ex
+++ lib/tictac.ex
@@ -1,18 +1,5 @@
 defmodule Tictac do
-  @moduledoc """
-  Documentation for `Tictac`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Tictac.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def squares do
+    for c<- 1..3, r <- 1..3, into: MapSet.new(), do: %Square{col: c, row: r}
   end
 end
